319d13c3b70be1b025f375c54b8783f79b5ba4a3
Jesse Fong <fong.jes@gmail.com>
2021-06-20 17:12:26 -0700

    Some Spelling Errors

--- minimal_bindless_gl.cpp
+++ minimal_bindless_gl.cpp
@@ -2,7 +2,7 @@
 // Minimal DSA/Bindless/Multidraw OpenGL by Jesse Fong
 // 
 // OpenGL is a Chonker of a spec at this point and while most people are moving to Vulkan and DX12,
-// I find that modern OpenGL is still usefull for personal projects becauseof its turseness and fire and forget approach to gpu 
+// I find that modern OpenGL is still usefull for personal projects because of its turseness and fire and forget approach to gpu memory
 // Finding reliable modern opengl resources can be difficult, so I wanted to make a sample app that brings a bit of it together.
 //
 // Here are some useful resources I have used in the past
